f72f07e48967942c2e58ff4e0a70b977643a2bf5
eliooooooo <you@example.com>
2023-11-21 12:21:12 +0100

    Upgrade update-form feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
 // import AdminToolbar from './admintoolbar.js';
 
-// import '../css/style.css';
+import '../css/style.css';
 // import './login.js';
+
+
+document.querySelectorAll('.toggle-input').forEach(function(checkbox) {
+  checkbox.addEventListener('change', function() {
+    this.nextElementSibling.style.display = this.checked ? 'block' : 'none';
+  });
+});
--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,8 @@
 (() => {
+  // assets/js/main.js
+  document.querySelectorAll(".toggle-input").forEach(function(checkbox) {
+    checkbox.addEventListener("change", function() {
+      this.nextElementSibling.style.display = this.checked ? "block" : "none";
+    });
+  });
 })();
