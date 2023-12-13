925d48c31066ae867ed1d09037b9325761f72259
eliooooooo <you@example.com>
2023-12-13 21:50:52 +0100

    Working custom audio viewer

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,10 +2,4 @@ import '../css/style.css';
 import './notification.js';
 import './admin-panel.js';
 import './swiper.js';
-
-
-document.querySelectorAll('.toggle-input').forEach(function(checkbox) {
-  checkbox.addEventListener('change', function() {
-    this.nextElementSibling.style.display = this.checked ? 'block' : 'none';
-  });
-});
+import './audio.js';
