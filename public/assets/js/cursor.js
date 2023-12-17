ee151b7b9e8ba2900d4d037337035dace9645b88
eliooooooo <you@example.com>
2023-12-17 20:19:10 +0100

    remove custom cursor

---
+++ public/assets/js/cursor.js
@@ -0,0 +1,35 @@
+let cursor = document.querySelector('.cursor');
+let cursorinner = document.querySelector('.cursor2');
+let a = document.querySelectorAll('a');
+
+document.addEventListener('mousemove', function(e){
+    var x = e.clientX;
+    var y = e.clientY;
+    cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+  });
+
+document.addEventListener('mousemove', function(e){
+    var x = e.clientX;
+    var y = e.clientY;
+    cursorinner.style.left = x + 'px';
+    cursorinner.style.top = y + 'px';
+  });
+
+document.addEventListener('mousedown', function(){
+    cursor.classList.add('click');
+    cursorinner.classList.add('cursorinnerhover')
+  });
+
+document.addEventListener('mouseup', function(){
+    cursor.classList.remove('click')
+    cursorinner.classList.remove('cursorinnerhover')
+  });
+
+a.forEach(item => {
+item.addEventListener('mouseover', () => {
+    cursor.classList.add('hover');
+});
+item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('hover');
+});
+})
