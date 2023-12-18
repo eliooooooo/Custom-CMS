d1406c4c6f3d47d5311a2493e83cb245ef003f8f
eliooooooo <you@example.com>
2023-12-18 18:53:00 +0100

    first blocks style

--- public/assets/js/audio.js
+++ public/assets/js/audio.js
@@ -1,40 +1,68 @@
-let progress = document.getElementById('progress');
-let audio = document.getElementById('song');
-let ctrlIcon = document.getElementsByClassName('ctrl-play-pause');
+document.addEventListener("DOMContentLoaded", () => {
 
-audio.onloadeddata = function() {
-    progress.max = audio.duration;
-    progress.value = audio.currentTime;
-}
+    let progress = document.querySelector('#progress');
+    let audio = document.querySelector('#song');
+    let ctrlIcon = document.querySelector('.ctrl-play-pause');
+    let start = document.querySelector('#start');
+    let end = document.querySelector('#end');
 
-window.toggleplay = function() {
-    if (ctrlIcon[0]) { // Vérifiez que ctrlIcon[0] n'est pas null
+    let toggleplay = document.querySelector('#toggleplay');
+    let playbackward = document.querySelector('#playbackward');
+    let playforward = document.querySelector('#playforward');
+
+
+
+    let dur = audio.duration;
+    let secD = Math.floor(dur % 60);
+    let minD = Math.floor(dur / 60);
+    end.innerHTML = minD + ":" + (secD < 10 ? "0" : "") + secD;
+
+
+    progress.max = audio.duration
+
+
+
+    toggleplay.addEventListener('click', () => {
+        console.log(audio.paused)
         if (audio.paused) {
             audio.play();
-            ctrlIcon[0].classList.toggle('hidden');
             ctrlIcon[1].classList.toggle('hidden');
-        } else {
-            audio.pause();
             ctrlIcon[0].classList.toggle('hidden');
-            ctrlIcon[1].classList.toggle('hidden');
         }
-    }
-}
+        else {
+            audio.pause();
+            ctrlIcon[1].classList.toggle('hidden');
+            ctrlIcon[0].classList.toggle('hidden');
+        }
+    })
 
-if (audio.play()) {
-    setInterval(function() {
-        progress.value = audio.currentTime;
-    }, 500);
-}
 
-progress.onchange = function() {
-    audio.currentTime = progress.value;
-}
+    audio.addEventListener('play', () => {
+        setInterval(function () {
+            progress.value = audio.currentTime;
+            console.log(progress.value, audio.currentTime)
+        }, 500);
+    })
 
-window.playforward = function() {
-    audio.currentTime += 10;
-}
+    progress.addEventListener('change', () => {
+        audio.currentTime = progress.value;
 
-window.playbackward = function() {
-    audio.currentTime -= 10;
-}
+    })
+
+
+
+    playforward.addEventListener('click', () => {
+        audio.currentTime += 10;
+    })
+
+    playbackward.addEventListener('click', () => {
+        audio.currentTime += 10;
+    })
+
+    audio.addEventListener('timeupdate', function () {
+        let currentTime = audio.currentTime;
+        let sec = Math.floor(currentTime % 60);
+        let min = Math.floor(currentTime / 60);
+        start.innerHTML = min + ":" + (sec < 10 ? "0" : "") + sec;
+    });
+});
